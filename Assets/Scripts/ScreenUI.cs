52da61f0ca9b8f87960a9dba6252ea5ae68b0427
YoheiShiina <ysys.j.0125@gmail.com>
2018-07-11 03:52:58 +0900

    失ったデータ修正。 通信機能実装の手前まで

---
+++ Assets/Scripts/ScreenUI.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class ScreenUI : MonoBehaviour {
+	private float _remainingTime;
+
+	[SerializeField] private Text[] text = new Text[4];
+	[SerializeField ]private ShootBullet shootBul;
+	[SerializeField] private TargetController targetCon;
+
+	// Use this for initialization
+	void Start () {
+		_remainingTime = 100;
+	}
+	// Update is called once per frame
+	void Update () {
+		_remainingTime -= 1*Time.deltaTime;
+
+		text[0].text = ("Time:"+_remainingTime.ToString("f1"));
+		text [1].text = ("Pt:"+ targetCon.TotalScore);
+		text[2].text = ("BulletBox:"+ shootBul.BulletBox);
+		text [3].text = ("Bullet:"+ shootBul.ChargedBullet);
+	}
+} 
