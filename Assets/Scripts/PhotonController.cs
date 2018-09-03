d33d82eb2163fe3198690e9da1a579180fad2e17
YoheiShiina <ysys.j.0125@gmail.com>
2018-09-03 16:02:38 +0900

    create room with a typed name

--- Assets/Scripts/PhotonController.cs
+++ Assets/Scripts/PhotonController.cs
@@ -1,28 +1,52 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using UnityEngine.UI;
 public class PhotonController : Photon.MonoBehaviour
 {
+	private string _roomName;
+	private LobbyAndRoomController lobbyAndRoomController;
+	[SerializeField]private GameObject lRController;
+	[SerializeField]private InputField lobbyInputField;
+
     // Use this for initialization
     void Start()
     {
-        PhotonNetwork.ConnectUsingSettings("0.1");
+		lobbyInputField = lobbyInputField.GetComponent<InputField> ();
+		lobbyAndRoomController = lRController.GetComponent<LobbyAndRoomController>();
+        PhotonNetwork.ConnectUsingSettings("0.1");	//自動ロビーログイン
+
     }
 
     void OnJoinedLobby()
     {
         Debug.Log("ロビーに入りました");
-        PhotonNetwork.JoinRandomRoom();
-    }
 
-    void OnPhotonRandomJoinFailed()
+    
+	}
+
+	public void MakeRoom(){ //ルーム作成
+		print("呼ばれた");
+		//変数にROおｍ名を代入
+		_roomName = lobbyInputField.text;
+		//Roomを作成
+		//_roomNameがnullならreturn
+		if (_roomName == "") {
+			return;
+		}
+		Debug.Log (_roomName);
+		PhotonNetwork.CreateRoom (_roomName);
+		　//入室画面表示
+		lobbyAndRoomController.OpenRoomPanel();
+	}
+
+    void OnPhotonCreateRoomFailed()//作成失敗
     {
         Debug.Log("ルーム入室に失敗しました");
         PhotonNetwork.CreateRoom(null);
     }
 
-    void OnJoinedRoom()
+    void OnJoinedRoom() //ルーム入室成功時よびだし
     {
         Debug.Log("ルームに入室しました");
 
@@ -32,6 +56,6 @@ public class PhotonController : Photon.MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-
+		
     }
 }
