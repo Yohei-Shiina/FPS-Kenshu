64b988a96220bf9694dc3fa13c5dae733d3be355
YoheiShiina <ysys.j.0125@gmail.com>
2018-09-05 20:13:08 +0900

    ルームをユーザーが作成できる機能実装

---
+++ Assets/Scripts/ButtonController.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class ButtonController : MonoBehaviour {
+
+	private string _roomName;
+	private GameObject parentObj;
+	private GameObject childObj;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public void EnterRoom()
+	{
+		parentObj = transform.parent.gameObject;
+		childObj = parentObj.transform.Find ("RoomText").gameObject;
+		_roomName = childObj.GetComponent<Text> ().text;
+		print (_roomName);
+
+		PhotonNetwork.JoinRoom (_roomName);
+
+	}
+}
--- Assets/Scripts/PhotonController.cs
+++ Assets/Scripts/PhotonController.cs
@@ -2,19 +2,38 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 public class PhotonController : Photon.MonoBehaviour
 {
+
 	private string _roomName;
-	private LobbyAndRoomController lobbyAndRoomController;
-	[SerializeField]private GameObject lRController;
+	private RoomOptions roomOptions; 
+	private RoomInfo[] roomInfo;
+	private GameObject roomtextObj;
+	private string _roomText;
+	private string _roomNameforJoin;
+	private bool _isConnected;
+	private Text stateText;
+	[SerializeField]private GameObject stateTextObj;
+	[SerializeField]private GameObject roomPanel;
 	[SerializeField]private InputField lobbyInputField;
+	[SerializeField]private Text[] roomPanelText;
+	[SerializeField]private GameObject[] roomPrefab;
+
 
     // Use this for initialization
     void Start()
     {
+		PhotonNetwork.ConnectUsingSettings("0.1");
+		stateText = stateTextObj.GetComponent<Text>();
+		stateText.text = "Lobby";
 		lobbyInputField = lobbyInputField.GetComponent<InputField> ();
-		lobbyAndRoomController = lRController.GetComponent<LobbyAndRoomController>();
-        PhotonNetwork.ConnectUsingSettings("0.1");	//自動ロビーログイン
+		print(lobbyInputField);
+		roomOptions = new RoomOptions ();
+		roomOptions.MaxPlayers = 4;
+		roomOptions.IsOpen = true;
+		roomOptions.IsVisible = true;
+		_isConnected = false;
 
     }
 
@@ -22,40 +41,85 @@ public class PhotonController : Photon.MonoBehaviour
     {
         Debug.Log("ロビーに入りました");
 
-    
 	}
 
-	public void MakeRoom(){ //ルーム作成
-		print("呼ばれた");
-		//変数にROおｍ名を代入
+	public void MakeRoom(){
 		_roomName = lobbyInputField.text;
-		//Roomを作成
-		//_roomNameがnullならreturn
-		if (_roomName == "") {
-			return;
-		}
-		Debug.Log (_roomName);
-		PhotonNetwork.CreateRoom (_roomName);
-		　//入室画面表示
-		lobbyAndRoomController.OpenRoomPanel();
+		print (_roomName);
+		PhotonNetwork.JoinOrCreateRoom (_roomName, roomOptions, null);
+
 	}
 
-    void OnPhotonCreateRoomFailed()//作成失敗
+
+
+    void OnPhotonCreateRoomFailed()
     {
         Debug.Log("ルーム入室に失敗しました");
         PhotonNetwork.CreateRoom(null);
     }
 
-    void OnJoinedRoom() //ルーム入室成功時よびだし
+
+	void OnReceivedRoomListUpdate(){
+		Text roomText;
+		Text playerText;
+
+		roomInfo = PhotonNetwork.GetRoomList ();
+
+		for (int i = 0; i < roomInfo.Length; i++) {
+
+			roomPrefab [i].SetActive (true);
+
+			roomText = roomPrefab [i].transform.Find ("RoomText").GetComponent<Text> ();
+			playerText = roomPrefab [i].transform.Find ("PlayerText").GetComponent<Text> ();
+		
+			roomText.text = roomInfo [i].Name;
+			playerText.text = "Player : " + roomInfo [i].PlayerCount;
+
+	
+		}
+	}
+
+    void OnJoinedRoom()
     {
-        Debug.Log("ルームに入室しました");
+        
+		Debug.Log("ルームに入室しました");
 
+		roomPanel.SetActive (true);
+		_isConnected = true;
+		print (roomInfo.Length);
+		//RoomPanelの部屋名の表示
+		roomPanelText [0].text = "Room Name : " + PhotonNetwork.room.Name;
     }
-
 
     // Update is called once per frame
     void Update()
-    {
-		
-    }
+	{
+		bool judgeRoom;
+		bool judgeSearch;
+
+		if (_isConnected) {
+			stateText.text = "Room";
+		} else {
+			stateText.text = "Lobby";
+		}
+
+		if (PhotonNetwork.playerList.Length < roomOptions.MaxPlayers) {
+			judgeRoom = true;
+			judgeSearch = true;
+		} else {
+			judgeRoom = false;
+			judgeSearch = false;
+		}
+		roomPanelText [1].text = "Player : " + PhotonNetwork.playerList.Length;
+		roomPanelText [2].text = "IsOpen : " + judgeRoom;
+		roomPanelText [3].text = "IsVisible : " + judgeSearch;
+	}
+
+
+	public void GameStart()
+	{
+		SceneManager.LoadScene ("Main");
+	}
+
+
 }
